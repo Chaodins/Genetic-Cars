14d0dd73fecde94e88bb64c51eebb43519adf5f8
kais <kais.mekdad@epitech.eu>
2019-02-11 19:07:11 +0100

    adding images and begin to set up the 2D environment.

--- p5/empty-example/sketch.js
+++ p5/empty-example/sketch.js
@@ -1,7 +1,15 @@
+var floor;
+
 function setup() {
-    createCanvas(400, 400);
+    createCanvas(1200, 800);
+    var img = loadImage('assets/floor.png');
+    imageSprite = createSprite(300, 700);
+    imageSprite.addImage(img);
+    imageSprite = createSprite(300 + 600, 700);
+    imageSprite.addImage(img);
 }
 
 function draw() {
     background(220);
+    drawSprites();
 }
